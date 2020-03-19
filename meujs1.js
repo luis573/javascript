411dfa2a83a70dacb5fccff3786c72c57f94bc8a
luis573 <luiscg573@hotmail.com>
2020-03-19 18:57:49 +0000

    prompt windows

--- meujs1.js
+++ meujs1.js
@@ -2,13 +2,19 @@ document.write("<h1>Olá mundo </h1><hr></br>");
 
 function MostrarCaixa()
 {
-    //alert("Olá pessoal");
-    var resultado=confirm("Então?");
-    if (resultado==true)
-        document.getElementById("zona").innerHTML="Tudo Ok";
-    else
-        document.getElementById("zona").innerHTML="Cancel";
+    //------------Alert e confirm box---------
+    // alert("Olá pessoal");
+    // var resultado=confirm("Então?");//devolve true ou false
+    // if (resultado==true)
+    //     document.getElementById("zona").innerHTML="Tudo Ok";
+    // else
+    //     document.getElementById("zona").innerHTML="Cancel";
 
+    //---------------propmt box
+    var nome = prompt("Qual é o teu \n nome?", "[escreva aqui]");
 
+    if (nome!=null && nome!="" && nome!="[escreva aqui]"){
+        document.getElementById("zona").innerHTML="Ola " + nome;
+    }
 
 }
